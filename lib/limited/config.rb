3b24b4e0a4a0396bf8a5c4e00944e246e44a71e2
Moritz Küttel <moritz.kuettel@gmail.com>
2013-06-09 17:00:59 +0200

    fixed RDoc in Limited::Config

--- lib/limited/config.rb
+++ lib/limited/config.rb
@@ -7,6 +7,10 @@ module Limited
 
     ##
     # adds a new limited action to the list of actions
+    # takes the same paramerers as Limited::Action.new
+    #
+    # raises an ArgumentError if the same name
+    # for an action is not unique
     def self.action(name, limit)
       action = Limited::Action.new(name, limit)
       raise ArgumentError.new("action with name :#{name.to_s} has already been added") if Limited.actions.has_key?(name)
@@ -14,6 +18,17 @@ module Limited
     end
   end
 
+  ##
+  # executes a block inside Limited::Config
+  # so you can easily access the methods in it
+  # and configure the Limited gem
+  #
+  # === Example
+  #
+  #   Limited.configure do
+  #     action :sending_contact_email, 50
+  #     action :failed_login, 1337
+  #   end
   def self.configure(&block)
     Config.instance_eval &block
   end

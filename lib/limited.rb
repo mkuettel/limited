29a3791785f866c56f28e484871ceb87d43224e0
Moritz Küttel <moritz.kuettel@gmail.com>
2013-06-09 16:52:49 +0200

    implemented magic methods for Limited

--- lib/limited.rb
+++ lib/limited.rb
@@ -1,5 +1,42 @@
 require "limited/version"
+require "limited/action"
 
 module Limited
-  # Your code goes here...
+  # a hash containing all the 
+  # limited actions
+  # the name of the Action is used as key
+  @actions = {}
+
+  ##
+  # returns a hash containing all the 
+  # limited actions
+  def self.actions
+    @actions
+  end
+
+
+  ##
+  # :call-seq:
+  #   Limited.<action_name> -> Limited::Action
+  #
+  # adds the possibility to access
+  # every action directly via Limited.<actionanem>
+  #
+  # === Example
+  #
+  #   Limited.configure do
+  #     action :some_action, 123
+  #   end
+  #
+  #   Limited.some_action.executed
+  #   Limited.some_action.num_left # -> 122
+  def self.method_missing(method_id, *arguments)
+    if @actions.has_key?(method_id)
+      @actions[method_id]
+    else
+      super
+    end
+  end
 end
+
+require "limited/config"

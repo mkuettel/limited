74264de57ccca1e981ff2a6eb5e8b7a80b131ad4
Moritz Küttel <moritz.kuettel@gmail.com>
2013-06-09 16:48:24 +0200

    added spec for Limited module

---
+++ spec/limited_spec.rb
@@ -0,0 +1,14 @@
+Limited.configure do
+  action :missing_method_test, 13
+end
+
+describe Limited do
+  it { Limited.should respond_to(:actions) }
+
+  describe "missing_method calls" do
+    it "should give access to the action object when calling method with its name" do
+      action = Limited.actions[:missing_method_test]
+      Limited.missing_method_test.should eq action
+    end
+  end
+end
